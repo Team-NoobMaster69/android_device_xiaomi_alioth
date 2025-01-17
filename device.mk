f2abdee4173b1337de6a77267920f0e16c815bd4
Danny Lin <danny@kdrag0n.dev>
2025-01-17 23:07:04 +0530

    alioth: overlay: Enable adaptive refresh rate
    Enable refresh rate toggle in Settings
    sweet supports switching between 60 and 120 Hz refresh rates, so let's
    expose it in Settings -> Display -> Smooth Display for users to save
    battery if necessary.
    
    Test: visual confirmation after toggling several times
    Change-Id: Ie698ec4d4e738afd2a9055dba2369233103a4f13
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Co-authored-by: SebaUbuntu <seba@sebaubuntu.dev>
    Signed-off-by: Vhmit <viktor.9630@protonmail.com>

--- device.mk
+++ device.mk
@@ -38,6 +38,8 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_KERNEL):kernel
 
 # Overlays
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
 PRODUCT_PACKAGES += \
     ApertureOverlayDevice \
     FrameworkResOverlayDevice \
